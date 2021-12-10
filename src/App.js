f2ec7f23e7c3db63b15a025f9ce36c4ec485690a
Andrew Gary <andrew.gary91@gmail.com>
2021-12-10 16:18:23 -0600

    my project is working as instructed but not passing the tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character.js';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +12,13 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [characters, setCharacters] = useState([]);
-  const [currentCharacter, setCurrentCharacter] = useState('1');
+  const [currentCharacter, setCurrentCharacter] = useState(null);
+
+  const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `
 
   const openCharacter = id => {
     setCurrentCharacter(id);
@@ -35,14 +42,14 @@ const App = () => {
   
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <AppContainer>
+      <h1 className="Header"> Star Wars Characters</h1>
       {
         characters.map((ch, index) => {
-          return <Character id={index} />
+          return <Character id={index} open={openCharacter} close={closeCharacter} name={ch.name} info={ch} currentCharacter={currentCharacter}/>
         })
-      }      
-    </div>
+      }
+    </AppContainer>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,53 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
-import styled, { keyframes } from 'styled-components';
+import React from 'react'
+import styled from 'styled-components';
 
 export default function Character(props) {
+    
     const name = props.name;
-    const key = props.key;
-   
-    console.log(props);
-    console.log(key);
+    const id = props.id
+    const currentCharacter = props.currentCharacter;
 
-    return <h1>{name}</h1>  
+    const StyledCharacter = styled.div`
+        color: red;
+        width: 60%;
+        h1{
+            background-color: rgba(0, 0, 0, .2);
+            
+            &:hover{
+                background-color: rgba(0, 0, 0, .1);
+            }
+        }
+
+        p {
+            padding-left: 5%;
+            font-weight: bold;
+        }
+        p span{
+            font-size: 2rem;
+            color: royalblue;
+            font-weight: heavy;
+        }
+    `
+
+    return (
+        
+        <StyledCharacter>
+        <h1 onClick={() => props.currentCharacter != null ? props.close() : props.open(props.id)}>{name}</h1>
+        {
+            currentCharacter === props.id ? (
+              <>
+                <p><span>Name:</span> {props.info.name}</p>
+                <p><span>Gender:</span> {props.info.gender}</p>
+                <p><span>Height:</span> {props.info.height}</p>
+                <p><span>Mass:</span> {props.info.mass}</p>
+                <p><span>BirthYear:</span> {props.info.birth_year}</p>
+                <p><span>Eye Color:</span> {props.info.eye_color}</p>
+                <p><span>Skin Color:</span> {props.info.skin_color}</p>
+              </>  
+            ) : null
+        }
+        </StyledCharacter>
+        
+    ) 
 }
