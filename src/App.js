5b61d4e6913d602d9573ac3e7c2f7f32be2b8a12
Andrew Gary <andrew.gary91@gmail.com>
2021-12-10 14:13:01 -0600

    working on getting codegrade to pass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character.js';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,38 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState('1');
+
+  const openCharacter = id => {
+    setCurrentCharacter(id);
+  }
+
+  const closeCharacter = id =>{
+    setCurrentCharacter(null);
+  }
+
+  useEffect(() => {
+    axios.get('http://swapi.dev/api/people')
+    .then(resp => {
+      // console.log(resp.data.results);
+      setCharacters(resp.data.results);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }, [])
+
+  
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((ch, index) => {
+          return <Character id={index} />
+        })
+      }      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import styled, { keyframes } from 'styled-components';
+
+export default function Character(props) {
+    const name = props.name;
+    const key = props.key;
+   
+    console.log(props);
+    console.log(key);
+
+    return <h1>{name}</h1>  
+}
